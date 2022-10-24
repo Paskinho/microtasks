3daaea575506db03f0004422d758e1b5ec2ac261
paskanov <v.paskanov@gmail.com>
2022-10-24 09:52:34 +0300

    Add Input. Коммент деньги

--- NewComponent.tsx
+++ NewComponent.tsx
@@ -1,30 +1,30 @@
-import React from 'react'
-import{NewComponentType} from "./App";
-
-
-export const NewComponent: NewComponentType =() => {
-    return (
-        <>
-            <ul>
-                {currentMoney.map((objFromMoneyArr, index )=>{
-                        return (
-                            <li key={index}>
-                                <span>{objFromMoneyArr.banknots}</span>
-                                <span>{objFromMoneyArr.value}</span>
-                                <span>{objFromMoneyArr.number}</span>
-                            </li>
-                        )
-
-                    }
-                )
-                }
-            </ul>
-            <div style={{marginLeft: '35px'}}>
-                <button onClick={()=>onClickFilterHandler('all')}>all</button>
-                <button onClick={()=>onClickFilterHandler('RUBLS')}>rubles</button>
-                <button onClick={()=>onClickFilterHandler('Dollars')}>dollars</button>
-            </div>
-        </>
-
-    )
-}
+// import React from 'react'
+// import{NewComponentType} from "./App";
+//
+//
+// export const NewComponent: NewComponentType =() => {
+//     return (
+//         <>
+//             <ul>
+//                 {currentMoney.map((objFromMoneyArr, index )=>{
+//                         return (
+//                             <li key={index}>
+//                                 <span>{objFromMoneyArr.banknots}</span>
+//                                 <span>{objFromMoneyArr.value}</span>
+//                                 <span>{objFromMoneyArr.number}</span>
+//                             </li>
+//                         )
+//
+//                     }
+//                 )
+//                 }
+//             </ul>
+//             <div style={{marginLeft: '35px'}}>
+//                 <button onClick={()=>onClickFilterHandler('all')}>all</button>
+//                 <button onClick={()=>onClickFilterHandler('RUBLS')}>rubles</button>
+//                 <button onClick={()=>onClickFilterHandler('Dollars')}>dollars</button>
+//             </div>
+//         </>
+//
+//     )
+// }
